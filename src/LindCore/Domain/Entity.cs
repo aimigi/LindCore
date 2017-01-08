9daa2aa0959cb1ef9a0281c4eac98dbe15072a31
Administrator <Administrator@2011-20170102GP>
2017-01-08 22:50:01 +0800

    cccccc

---
+++ src/LindCore/Domain/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LindCore
+{
+    /// <summary>
+    /// 领域实体基类
+    /// </summary>
+    /// <typeparam name="Key">主键类型</typeparam>
+    public class Entity<Key>
+    {
+        /// <summary>
+        /// 统一主键
+        /// </summary>
+       public Key Id { get; set; }
+    }
+}
