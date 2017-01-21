59f8903c1ba8a084062d71661dd4173668e2531f
bfyxzls <bfyxzls@sina.com>
2017-01-21 22:16:28 +0800

    添加了redis实例提供者

---
+++ src/LindCore/Commons/DisposableBase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LindCore.Commons
+{
+    /// <summary>
+    /// 实现IDisposable，对非托管系统进行资源回收
+    /// 作者：仓储大叔
+    /// </summary>
+    public abstract class DisposableBase : IDisposable
+    {
+
+        private bool _isDisposed;
+        /// <summary>
+        /// 由子类自己去实现自己的Dispose逻辑（清理托管和非托管资源）
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected abstract void Finalize(bool disposing);
+        /// <summary>
+        /// 标准Dispose，外界可以直接调用它
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);////释放托管资源
+            GC.SuppressFinalize(this);//请求系统不要调用指定对象的终结器. //该方法在对象头中设置一个位，系统在调用终结器时将检查这个位
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_isDisposed)//_isDisposed为false表示没有进行手动dispose
+            {
+                //清理托管资源和清理非托管资源
+                Finalize(disposing);
+            }
+            _isDisposed = true;
+        }
+        /// <summary>
+        /// 是否完成了资源的释放
+        /// </summary>
+        public bool IsDisposed
+        {
+            get { return this._isDisposed; }
+        }
+        /// <summary>
+        /// 析构方法－在类被释放前被执行
+        /// </summary>
+        ~DisposableBase()
+        {
+
+            //执行到这里，托管资源已经被释放
+            this.Dispose(false);//释放非托管资源，托管资源由终极器自己完成了
+        }
+    }
+}
---
+++ src/LindCore/RedisClient/RedisProvider.cs
@@ -0,0 +1,67 @@
+﻿using LindCore.Commons;
+using LindCore.Logger;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LindCore.RedisClient
+{
+    /// <summary>
+    /// Redis提供者
+    /// 实例－实现方式
+    /// </summary>
+    public class RedisProvider : DisposableBase
+    {
+        ConnectionMultiplexer redis;
+        ConfigurationOptions option;
+        /// <summary>
+        /// 生产一个redis连接实例
+        /// </summary>
+        /// <param name="conn">连接串，可以是sentinel集合</param>
+        /// <param name="password">redis数据服务器密码</param>
+        /// <param name="serviceName">sentinel模式下的服务名称</param>
+        /// <param name="isSentinel">是否连接为sentinel</param>
+        public RedisProvider(
+            string conn,
+            string password = "",
+            string serviceName = "",
+            bool isSentinel = false)
+        {
+            option = new ConfigurationOptions();
+            var configArr = conn.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+            configArr.ToList().ForEach(i => option.EndPoints.Add(i));
+            option.TieBreaker = "";//这行在sentinel模式必须加上
+            option.CommandMap = CommandMap.Sentinel;
+            redis = ConnectionMultiplexer.Connect(option);
+
+            for (int i = 0; i < option.EndPoints.Count; i++)
+            { 
+                try
+                {
+                    conn = redis.GetServer(option.EndPoints[i]).SentinelGetMasterAddressByName(serviceName).ToString();
+                    break;
+                }
+                catch (RedisConnectionException ex)//超时
+                {
+                    LoggerFactory.Logger_Debug("RedisConnectionException" + ex.Message);
+                    continue;
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+            }
+        }
+
+        protected override void Finalize(bool disposing)
+        {
+            if (!disposing)
+            {
+                redis.Close();
+                redis.Dispose();
+            }
+        }
+    }
+}
